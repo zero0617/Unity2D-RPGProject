206fa7bb01c360f11df69d5aac9440f0d4901afb
zero0617 <86754891+zero0617@users.noreply.github.com>
2025-09-26 14:28:44 +0800

    背景移动

--- Assets/Scripts/ParallaxBcakground.cs
+++ Assets/Scripts/ParallaxBcakground.cs
@@ -8,29 +8,42 @@ public class ParallaxBcakground : MonoBehaviour
 {
     private Camera mainCamera;  //主相机
     private float lastCameraPositionX;  //相机x的最后位置
+    private float cameraHalfWidth;  //相机一半宽度
 
     [SerializeField] private ParallaxLayer[] backgroundLayers;
 
     private void Awake()
     {
         mainCamera = Camera.main;
+        cameraHalfWidth = mainCamera.orthographicSize * mainCamera.aspect;
+        CalculateImageLength();
     }
+
 
     private void Update()
     {
-        //初始化每帧当前相机x位置
-        float currentCameraPositionX = mainCamera.transform.position.x;
+        
+        float currentCameraPositionX = mainCamera.transform.position.x; //初始化每帧当前相机x位置   
+        float distanceToMove = currentCameraPositionX - lastCameraPositionX;   //每帧相机移动的距离   
+        lastCameraPositionX = currentCameraPositionX;   //更新相机最后位置
 
-        //每帧相机移动的距离
-        float distanceToMove = currentCameraPositionX - lastCameraPositionX;
-
-        //更新相机最后位置
-        lastCameraPositionX = currentCameraPositionX;
+        float cameraLeftEdge = currentCameraPositionX - cameraHalfWidth;    //相机左边界
+        float cameraRightEdge = currentCameraPositionX + cameraHalfWidth;   //相机右边界
 
         //给每个背景添加移动方法
-        foreach(ParallaxLayer layer in backgroundLayers)
+        foreach (ParallaxLayer layer in backgroundLayers)
         {
             layer.Move(distanceToMove);
+            layer.LoopBackground(cameraLeftEdge, cameraRightEdge);
+        }
+    }
+
+    //计算图片长度
+    private void CalculateImageLength()
+    {
+        foreach(ParallaxLayer layer in backgroundLayers)
+        {
+            layer.CalculateImageWidth();
         }
     }
 }
--- Assets/Scripts/ParallaxLayer.cs
+++ Assets/Scripts/ParallaxLayer.cs
@@ -8,13 +8,40 @@ using UnityEngine;
 public class ParallaxLayer {
 
     [SerializeField] private Transform background;  //背景对象
-    [SerializeField] private float parallaxMultiplier; //背景移动速度
+    [SerializeField] private float parallaxMultiplier;  //背景移动速度
+    [SerializeField] private float imageWidthOffset = 10;    //填充相机背景间误差距离
 
+    private float imageFullWidth;   //背景全宽
+    private float imageHalfWidth;   //背景半宽
 
+    //背景移动
     public void Move (float distanceToMove)
     {
         //background.position = background.position + new Vector3(distanceToMove * parallaxMultiplier, 0);
         background.position += Vector3.right * (distanceToMove * parallaxMultiplier);
+    }
+
+    //计算背景宽度
+    public void CalculateImageWidth()
+    {
+        imageFullWidth = background.GetComponent<SpriteRenderer>().bounds.size.x;
+        imageHalfWidth = imageFullWidth / 2;
+    }
+
+
+    //背景循环
+    public void LoopBackground(float cameraLeftEdge,float cameraRightEdge)
+    {
+        float imageRightEdge = (background.position.x + imageHalfWidth) - imageWidthOffset;  //背景右边界
+        float imageLeftEdge = (background.position.x - imageHalfWidth) + imageWidthOffset;   //背景左边界
+
+        //如果背景左边界小于相机右边界，背景向右移动一个背景距离
+        if (imageRightEdge < cameraLeftEdge)
+            background.position += Vector3.right * imageFullWidth;
+        //如果背景左边界大于于相机右边界，背景向左移动一个背景距离
+        else if (imageLeftEdge > cameraRightEdge)
+            background.position += Vector3.right * -imageFullWidth;
+            
     }
 
 }
